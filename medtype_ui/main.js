cb04759c518bee4d3e282333c1008e479befab9b
Shikhar Vashishth <shikharvashishth@gmail.com>
2020-06-01 12:02:07 -0400

    updated

--- medtype_ui/main.js
+++ medtype_ui/main.js
@@ -206,7 +206,8 @@ function call_linker(){
         contentType: "application/json",
         error: function(e) {
             $.notify("Error encoutered while accessing the server");
-            alert('Need to accept the certificate before accessing the demo. Press Ok to continue. Come back to this page once done!')
+            alert('Need to accept the self-signed certificate first before accessing the demo. Press Ok to continue. Come back to this page once done! \
+                In case your browser does not give the option to accept the certificate, please try with another browser. Thanks!')
             window.open("https://128.2.204.127:8125/run_linker");
         },
     });
